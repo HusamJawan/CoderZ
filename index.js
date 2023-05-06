e43dc129574c480ce66b50dc338e2493ead833c6
WebService <36390873+HusamJawan@users.noreply.github.com>
2023-05-06 17:38:46 +0400

    Add files via upload
    The files that contain the code have been uploaded.
    The site did not allow me to upload folders, only files, so the important files that contain the code were uploaded.
    in addition to a screenshot.png for the user interface after running the program.

---
+++ index.js
@@ -0,0 +1,14 @@
+//This file sets up the Redux store and renders the App component.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
